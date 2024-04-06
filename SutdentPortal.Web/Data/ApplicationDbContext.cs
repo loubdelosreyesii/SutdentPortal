635ce54a0765c7f4fcadc413a719f0308491ae6a
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2024-04-06 14:32:09 +0800

    Add project files.

---
+++ SutdentPortal.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SutdentPortal.Web.Models.Entities;
+
+namespace SutdentPortal.Web.Data
+{
+    //Inherit DBContext Class
+    public class ApplicationDbContext : DbContext
+    {
+        //type ctor to create constructor class
+        //Bridge between Application and Sql Server Database.
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Representation of collection of your entity
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ SutdentPortal.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SutdentPortal.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
